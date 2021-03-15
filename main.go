f80db59d45343e9f8d13555a68e5a0198ebda3ac
task4233 <tks.m1205@gmail.com>
2021-03-16 01:45:50 +0900

    change: main.goのポート番号をconfigパッケージを用いて取得する形式に変更

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/openhacku-saboten/OmnisCode-backend/config"
 )
 
 func main() {
@@ -11,5 +13,5 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(":8080"))
+	e.Logger.Fatal(e.Start(fmt.Sprintf(":%s", config.Port())))
 }
