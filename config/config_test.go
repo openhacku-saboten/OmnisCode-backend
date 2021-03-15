9e67e262b47b537ec2348e9b8bfe00141a12333a
task4233 <tks.m1205@gmail.com>
2021-03-16 01:38:47 +0900

    feat: 環境変数等を管理するためにconfigパッケージを追加

---
+++ config/config_test.go
@@ -0,0 +1,49 @@
+package config_test
+
+import (
+	"testing"
+
+	"github.com/openhacku-saboten/OmnisCode-backend/config"
+)
+
+func TestPort(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "正しくポートを取得できる",
+			want: "8080",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			if got := config.Port(); got != tc.want {
+				t.Errorf("Port() = %s, want = %s", got, tc.want)
+			}
+		})
+	}
+}
+
+func TestDSN(t *testing.T) {
+	t.Parallel()
+	cases := []struct {
+		name string
+		want string
+	}{
+		{
+			name: "正しくDSNを取得できる",
+			want: "test:test@tcp(127.0.0.1:3306)/test?parseTime=true&collation=utf8mb4_bin",
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			if got := config.DSN(); got != tc.want {
+				t.Errorf("DSN() = %s, want = %s", got, tc.want)
+			}
+		})
+	}
+}
