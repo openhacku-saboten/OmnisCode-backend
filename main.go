dacf6001b942b03c7384af5c677738da9cafaf2e
task4233 <tks.m1205@gmail.com>
2021-03-16 21:25:26 +0900

    feat: main.goでLoggerが機能していることを確認

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ func main() {
 
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
+		logger.Infof("Access from %s", c.Request().URL)
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
