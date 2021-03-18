d31abfc20ab8a2e0608509e222e96f4f0555d461
task4233 <tks.m1205@gmail.com>
2021-03-18 14:19:13 +0900

    feat: example_testを追加

--- infra/database_test.go
+++ infra/database_test.go
@@ -1,6 +1,8 @@
 package infra_test
 
 import (
+	"os"
+
 	"github.com/openhacku-saboten/OmnisCode-backend/infra"
 	"github.com/openhacku-saboten/OmnisCode-backend/log"
 )
@@ -9,8 +11,17 @@ import (
 func ExampleNewDB() {
 	logger := log.New()
 
-	db, err := infra.NewDB()
+	dbMap, err := infra.NewDB()
 	if err != nil {
-		logger.Fatal(err)
+		logger.Errorf("failed NewDB: %s", err.Error())
+		os.Exit(1)
 	}
+
+	// きちんとCloseすること
+	defer func() {
+		err := dbMap.Db.Close()
+		if err != nil {
+			logger.Errorf("failed to close DB: %s", err.Error())
+		}
+	}()
 }
