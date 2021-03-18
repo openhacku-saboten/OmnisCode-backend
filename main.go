53ae38cf2122645bc412131522014c5328bad710
Hiroya Onoe <onoehiroya@gmail.com>
2021-03-18 12:54:27 +0900

    feat: 認証の必要なアクセスが出来る例(/secret)を追加

--- main.go
+++ main.go
@@ -7,16 +7,31 @@ import (
 
 	"github.com/labstack/echo/v4"
 	"github.com/openhacku-saboten/OmnisCode-backend/config"
+	"github.com/openhacku-saboten/OmnisCode-backend/controller"
+	"github.com/openhacku-saboten/OmnisCode-backend/infra"
 	"github.com/openhacku-saboten/OmnisCode-backend/log"
+	"github.com/openhacku-saboten/OmnisCode-backend/usecase"
 )
 
 func main() {
 	logger := log.New()
 
+	firebase := infra.NewFirebase()
+	authUseCase := usecase.NewAuthUseCase(firebase)
+	authMiddleware := controller.NewAuthMiddleware(authUseCase)
+
 	e := echo.New()
+
 	e.GET("/", func(c echo.Context) error {
 		logger.Infof("Access from %s", c.Request().RemoteAddr)
 		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	authed := e.Group("/secret", authMiddleware.Authenticate)
+
+	authed.GET("", func(c echo.Context) error {
+		logger.Infof("Authorized access from%s", c.Request().RemoteAddr)
+		return c.String(http.StatusOK, c.Get("userID").(string))
 	})
 
 	if err := e.Start(fmt.Sprintf(":%s", config.Port())); err != nil {
