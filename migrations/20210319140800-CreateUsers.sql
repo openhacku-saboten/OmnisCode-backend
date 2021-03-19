33ebac9f86b61978cd37cf7ff166c53677c86360
Hiroya Onoe <onoehiroya@gmail.com>
2021-03-19 17:06:28 +0900

    style: sqlのフォーマットを整えた

--- migrations/20210319140800-CreateUsers.sql
+++ migrations/20210319140800-CreateUsers.sql
@@ -1,10 +1,10 @@
 
 -- +migrate Up
 CREATE TABLE IF NOT EXISTS users (
-    id VARCHAR(128) PRIMARY KEY,
-    name VARCHAR(128) NOT NULL,
+    id         VARCHAR(128) PRIMARY KEY,
+    name       VARCHAR(128) NOT NULL,
     twitter_id VARCHAR(15),
-    profile TEXT,
+    profile    TEXT,
     UNIQUE KEY (twitter_id)
 );
 -- +migrate Down
