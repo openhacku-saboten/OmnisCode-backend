ee14dd86ee253b69f374bedcd18410ac073ba876
Hiroya Onoe <onoehiroya@gmail.com>
2021-03-19 23:25:25 +0900

    feat(usecase/user.go): iconURLをauthRepoから取得して設定するようにした

--- usecase/user.go
+++ usecase/user.go
@@ -1,19 +1,31 @@
 package usecase
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/openhacku-saboten/OmnisCode-backend/domain/entity"
 	"github.com/openhacku-saboten/OmnisCode-backend/repository"
 )
 
 type UserUseCase struct {
 	userRepo repository.User
+	authRepo repository.Auth
 }
 
-func NewUserUseCase(user repository.User) *UserUseCase {
-	return &UserUseCase{userRepo: user}
+func NewUserUseCase(user repository.User, auth repository.Auth) *UserUseCase {
+	return &UserUseCase{userRepo: user, authRepo: auth}
 }
 
-func (u *UserUseCase) Get(uid string) (user *entity.User, err error) {
+func (u *UserUseCase) Get(ctx context.Context, uid string) (user *entity.User, err error) {
 	user, err = u.userRepo.FindByID(uid)
+	if err != nil {
+		return nil, fmt.Errorf("failed to Get: %w", err)
+	}
+
+	user.IconURL, err = u.authRepo.GetIconURL(ctx, uid)
+	if err != nil {
+		return nil, fmt.Errorf("failed to Get: %w", err)
+	}
 	return
 }
