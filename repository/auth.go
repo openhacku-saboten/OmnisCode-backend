9fde5d381b6bd16791f43bf14a0eab6055524c11
Hiroya Onoe <onoehiroya@gmail.com>
2021-03-19 23:25:15 +0900

    feat(infr/auth.go): firebaseからiconURLを取得する処理を実装

--- repository/auth.go
+++ repository/auth.go
@@ -7,4 +7,5 @@ import "context"
 // Auth はFirebase Authentication関連の操作を表すインターフェース
 type Auth interface {
 	Authenticate(ctx context.Context, token string) (uid string, err error)
+	GetIconURL(ctx context.Context, uid string) (iconURL string, err error)
 }
