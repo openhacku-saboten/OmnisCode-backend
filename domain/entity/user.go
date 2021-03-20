9751e7ec85a0f487ade9240996ed03d4d3355a84
Hiroya Onoe <onoehiroya@gmail.com>
2021-03-20 11:05:41 +0900

    fix(domain/entity/user.go): JSONにエンコードするときのフィールド名を指定

--- domain/entity/user.go
+++ domain/entity/user.go
@@ -1,11 +1,11 @@
 package entity
 
 type User struct {
-	ID        string
-	Name      string
-	Profile   string
-	TwitterID string
-	IconURL   string
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	Profile   string `json:"profile"`
+	TwitterID string `json:"twitter_id"`
+	IconURL   string `json:"icon_url"`
 }
 
 func NewUser(id, name, profile, twitterID, iconURL string) *User {
