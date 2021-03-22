5ef0e9cf897a7d7e603b0b447fc811c6a69a2baa
Hiroya Onoe <onoehiroya@gmail.com>
2021-03-22 16:51:46 +0900

    test(controller/user_test.go): createテスト追加

--- controller/user_test.go
+++ controller/user_test.go
@@ -162,6 +162,14 @@ func TestUserController_Create(t *testing.T) {
 			wantCode:        400,
 		},
 		{
+			name:            "bodyがJSON形式でないならBadRequest",
+			userID:          "user-id",
+			body:            `aaaaa`,
+			prepareMockUser: func(user *mock.MockUser) {},
+			wantErr:         true,
+			wantCode:        400,
+		},
+		{
 			name:   "userIDが重複しているならBadRequest",
 			userID: "user-id",
 			body: `{
