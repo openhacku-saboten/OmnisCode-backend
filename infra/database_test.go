96b2c6341610cb320a4f34c314e84652601acfd0
task4233 <tks.m1205@gmail.com>
2021-03-23 17:23:46 +0900

    feat: テーブルをtruncateするヘルパ関数を一般化

--- infra/database_test.go
+++ infra/database_test.go
@@ -1,15 +1,17 @@
-package infra_test
+package infra
 
 import (
+	"fmt"
 	"os"
 	"testing"
+	"time"
 
-	"github.com/openhacku-saboten/OmnisCode-backend/infra"
+	"github.com/go-gorp/gorp"
 	"github.com/openhacku-saboten/OmnisCode-backend/log"
 )
 
 func TestNewDB(t *testing.T) {
-	dbMap, err := infra.NewDB()
+	dbMap, err := NewDB()
 	if err != nil {
 		t.Errorf("failed NewDB: %s", err.Error())
 	}
@@ -26,7 +28,7 @@ func TestNewDB(t *testing.T) {
 func ExampleNewDB() {
 	logger := log.New()
 
-	dbMap, err := infra.NewDB()
+	dbMap, err := NewDB()
 	if err != nil {
 		logger.Errorf("failed NewDB: %s", err.Error())
 		os.Exit(1)
@@ -39,4 +41,28 @@ func ExampleNewDB() {
 			logger.Errorf("failed to close DB: %s", err.Error())
 		}
 	}()
+}
+
+// truncateTable は指定したテーブルをtruncateするヘルパ関数です
+func truncateTable(t *testing.T, dbMap *gorp.DbMap, tableName string) {
+	t.Helper()
+
+	// databaseを初期化する
+	if _, err := dbMap.Exec("SET FOREIGN_KEY_CHECKS = 0"); err != nil {
+		t.Fatal(err)
+	}
+	// タイミングの問題でTruncateが失敗することがあるので成功するまで試みる
+	for i := 0; i < 5; i++ {
+		_, err := dbMap.Exec(fmt.Sprintf("TRUNCATE TABLE %s", tableName))
+		if err == nil {
+			break
+		}
+		if i == 4 {
+			t.Fatal(err)
+		}
+		time.Sleep(time.Second * 1)
+	}
+	if _, err := dbMap.Exec("SET FOREIGN_KEY_CHECKS = 1"); err != nil {
+		t.Fatal(err)
+	}
 }
