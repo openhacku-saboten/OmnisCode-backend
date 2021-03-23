9c459bee98036b61bb2a93a846aa1745e054ce4f
task4233 <tks.m1205@gmail.com>
2021-03-24 02:55:10 +0900

    fix: デバッグ用のメッセージを削除

--- controller/post.go
+++ controller/post.go
@@ -43,5 +43,5 @@ func (ctrl *PostController) Create(c echo.Context) error {
 		return echo.NewHTTPError(http.StatusInternalServerError)
 	}
 
-	return c.String(http.StatusCreated, "hogefugapiyo")
+	return c.NoContent(http.StatusCreated)
 }
