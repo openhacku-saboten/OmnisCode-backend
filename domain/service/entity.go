0aa424ef91eb55bfa9935da0ada3b2f48c57d87d
task4233 <tks.m1205@gmail.com>
2021-03-24 13:47:31 +0900

    feat: CreateJSONのコメントを追加

--- domain/service/entity.go
+++ domain/service/entity.go
@@ -6,8 +6,9 @@ import (
 	"os"
 )
 
-// CreateJsonは構造体をJSONにした時のフォーマットを確認するための関数です
-func CreateJson(fileName string, entityData interface{}) error {
+// CreateJSONは構造体をJSONにした時のフォーマットを確認するための関数です
+// 実際のレスポンスがAPI仕様と同じかを確認するために利用することを想定しています
+func CreateJSON(fileName string, entityData interface{}) error {
 	fp, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 0600)
 	if err != nil {
 		return fmt.Errorf("failed to open file: %s: %w", fileName, err)
