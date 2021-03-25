29007f444f1908cf30eb9d7f51e6a010439aad76
Hiroya Onoe <onoehiroya@gmail.com>
2021-03-25 15:45:55 +0900

    test(infra/comment_test.go): FindByIDのテスト追加

--- infra/comment_test.go
+++ infra/comment_test.go
@@ -273,3 +273,110 @@ func TestCommentRepository_Create(t *testing.T) {
 		})
 	}
 }
+
+func TestCommentRepository_FindByID(t *testing.T) {
+	dbMap, err := NewDB()
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	dbMap.AddTableWithName(UserDTO{}, "users")
+	truncateTable(t, dbMap, "users")
+
+	if err := dbMap.Insert(&UserDTO{
+		ID:        "user-id",
+		Name:      "test user",
+		Profile:   "test profile",
+		TwitterID: "twitter",
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	dbMap.AddTableWithName(PostDTO{}, "posts").SetKeys(true, "id")
+	truncateTable(t, dbMap, "posts")
+
+	if err := dbMap.Insert(&PostDTO{
+		ID:        1,
+		UserID:    "user-id",
+		Title:     "test title",
+		Code:      "package main\n\nimport \"fmt\"\n\nfunc main(){fmt.Println(\"This is test.\")}",
+		Language:  "Go",
+		Content:   "Test code",
+		Source:    "github.com",
+		CreatedAt: time.Unix(100, 0),
+		UpdatedAt: time.Unix(100, 0),
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	dbMap.AddTableWithName(CommentDTO{}, "comments").SetKeys(true, "id")
+	truncateTable(t, dbMap, "comments")
+
+	if err := dbMap.Insert(&CommentDTO{
+		ID:        1,
+		UserID:    "user-id",
+		PostID:    1,
+		Type:      "none",
+		Content:   "type none",
+		CreatedAt: time.Unix(100, 0),
+		UpdatedAt: time.Unix(100, 0),
+	}); err != nil {
+		t.Fatal(err)
+	}
+
+	commentRepo := NewCommentRepository(dbMap)
+
+	tests := []struct {
+		name        string
+		postID      int
+		commentID   int
+		wantComment *entity.Comment
+		wantErr     error
+	}{
+		{
+			name:      "正しくコメントを取得できる",
+			postID:    1,
+			commentID: 1,
+			wantComment: &entity.Comment{
+				ID:        1,
+				UserID:    "user-id",
+				PostID:    1,
+				Type:      "none",
+				Content:   "type none",
+				CreatedAt: "1970-01-01T00:01:40+09:00",
+				UpdatedAt: "1970-01-01T00:01:40+09:00",
+			},
+			wantErr: nil,
+		},
+		{
+			name:        "PostIDが存在しなければErrNotFound",
+			postID:      100,
+			commentID:   1,
+			wantComment: nil,
+			wantErr:     entity.NewErrorNotFound("comment"),
+		},
+		{
+			name:        "CommentIDが存在しなければErrNotFound",
+			postID:      1,
+			commentID:   100,
+			wantComment: nil,
+			wantErr:     entity.NewErrorNotFound("comment"),
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			gotComment, err := commentRepo.FindByID(tt.postID, tt.commentID)
+
+			if !errors.Is(err, tt.wantErr) {
+				t.Errorf("error = %v, wantErr = %v", err, tt.wantErr)
+				return
+			}
+			if tt.wantErr == nil {
+				if diff := cmp.Diff(tt.wantComment, gotComment); diff != "" {
+					t.Errorf("Data (-want +got) =\n%s\n", diff)
+				}
+			}
+		})
+	}
+}
