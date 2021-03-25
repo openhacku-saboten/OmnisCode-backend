881a3c5cde61b13c0962dd0383a29816297c7662
task4233 <tks.m1205@gmail.com>
2021-03-25 20:54:24 +0900

    feat: repositoryにメソッドを追加

--- repository/post.go
+++ repository/post.go
@@ -14,4 +14,5 @@ type Post interface {
 	FindByID(ctx context.Context, postID int) (*entity.Post, error)
 	FindByUserID(ctx context.Context, uid string) ([]*entity.Post, error)
 	Insert(ctx context.Context, post *entity.Post) error
+	Update(ctx context.Context, post *entity.Post) error
 }
