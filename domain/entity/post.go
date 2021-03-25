8130609d75330b59e8f2a0fbdcc4f1465f137eb2
task4233 <tks.m1205@gmail.com>
2021-03-25 23:52:36 +0900

    feat(post): post entityに状態を追加

--- domain/entity/post.go
+++ domain/entity/post.go
@@ -2,11 +2,25 @@ package entity
 
 import (
 	"errors"
+	"strconv"
 )
 
 // Post は投稿を表します
 type Post struct {
 	ID        int    `json:"id"`
+	UserID    string `json:"user_id"`
+	Title     string `json:"title"`
+	Code      string `json:"code"`
+	Language  string `json:"language"`
+	Content   string `json:"content"`
+	Source    string `json:"source"`
+	CreatedAt string `json:"created_at"`
+	UpdatedAt string `json:"updated_at"`
+}
+
+// PostForJSON はJSONにBindする時のPostです
+type PostForJSON struct {
+	ID        string `json:"id"`
 	UserID    string `json:"user_id"`
 	Title     string `json:"title"`
 	Code      string `json:"code"`
@@ -48,5 +62,39 @@ func (p *Post) IsValid() error {
 	}
 
 	return nil
+}
 
+// Convert はPostForJSONからPostに変換します
+func (p PostForJSON) Convert() (*Post, error) {
+	id, err := strconv.Atoi(p.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Post{
+		ID:        id,
+		UserID:    p.UserID, // c.Get("userID")で更新される
+		Title:     p.Title,
+		Code:      p.Code,
+		Language:  p.Language,
+		Content:   p.Content,
+		Source:    p.Source,
+		CreatedAt: p.CreatedAt, // 永続化する時に自動更新される
+		UpdatedAt: p.UpdatedAt, // 永続化する時に自動更新される
+	}, nil
+}
+
+// Convert はPostからPostForJSONに変換します
+func (p Post) Convert() *PostForJSON {
+	return &PostForJSON{
+		ID:        strconv.Itoa(p.ID),
+		UserID:    p.UserID, // c.Get("userID")で更新される
+		Title:     p.Title,
+		Code:      p.Code,
+		Language:  p.Language,
+		Content:   p.Content,
+		Source:    p.Source,
+		CreatedAt: p.CreatedAt, // 永続化する時に自動更新される
+		UpdatedAt: p.UpdatedAt, // 永続化する時に自動更新される
+	}
 }
